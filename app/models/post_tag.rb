2285ac62e2e1ad659ae1c1a56de132f9c115788b
Erick <erisrnb18@gmail.com>
2021-09-03 14:37:29 -0400

    done

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,2 +1,13 @@
-class PostTag < ApplicationRecord
-end
+class Post < ApplicationRecord
+    belongs_to :author
+    has_many :post_tags
+  end
+  
+  class PostTag < ApplicationRecord
+    belongs_to :post
+    belongs_to :tag
+  end
+  
+  class Tag < ApplicationRecord
+    has_many :post_tags
+  end
